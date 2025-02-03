b3cefefcf322dc8d055b037563bcfa016955ba87
Jason Mercer <jasonjmercer@gmail.com>
2025-02-02 21:23:40 -0500

    Adding notebooks largely from module 2

--- scripts/get_wd.py
+++ scripts/get_wd.py
@@ -1,3 +1,29 @@
 import os
 
 os.getcwd()
+
+my_tuple = (1, "b", 3.0)
+type(my_tuple)
+len(my_tuple)
+
+# Aliasing lists
+A = ["banana"]
+B = A
+A[0] = "split"
+B # Result: ["split"]
+
+# Cloning lists
+A = ["banana"]
+B = A[:]
+A[0] = ["split"]
+B # Result: ["banana"]
+
+my_dict = {1: "A", "B": 3}
+my_dict
+
+my_dict[1] # "A"
+my_dict[0] # Error
+0 in my_dict # False
+1 in my_dict # True
+
+list(my_dict.keys())
